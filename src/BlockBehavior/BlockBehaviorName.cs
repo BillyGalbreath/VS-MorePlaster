30db971c3851a3a420978116c7f84b2568d2e18e
William Blake Galbreath <Blake.Galbreath@GMail.com>
2024-01-28 13:38:20 -0600

    update to 1.19

--- src/BlockBehavior/BlockBehaviorName.cs
+++ src/BlockBehavior/BlockBehaviorName.cs
@@ -7,29 +7,29 @@ using Vintagestory.API.MathTools;
 namespace MorePlaster.BlockBehavior;
 
 public class BlockBehaviorName : Vintagestory.API.Common.BlockBehavior {
-    private string? name;
+    private string? _name;
 
     public BlockBehaviorName(Block block) : base(block) { }
 
     public override void Initialize(JsonObject properties) {
         base.Initialize(properties);
 
-        name = $"{Lang.Get(properties["material"].AsString())} {Lang.GetMatchingIfExists(properties["name"].AsString())}";
+        _name = $"{Lang.Get(properties["material"].AsString())} {Lang.GetMatchingIfExists(properties["name"].AsString())}";
     }
 
     public override void GetHeldItemName(StringBuilder sb, ItemStack itemStack) {
-        if (name == null) {
+        if (_name == null) {
             return;
         }
 
-        sb.Clear().Append(name);
+        sb.Clear().Append(_name);
     }
 
     public override void GetPlacedBlockName(StringBuilder sb, IWorldAccessor world, BlockPos pos) {
-        if (name == null) {
+        if (_name == null) {
             return;
         }
 
-        sb.Clear().Append(name);
+        sb.Clear().Append(_name);
     }
 }
--- src/MorePlasterMod.cs
+++ src/MorePlasterMod.cs
@@ -1,12 +1,12 @@
-﻿using MorePlaster.BlockBehavior;
+﻿using System.Diagnostics.CodeAnalysis;
+using MorePlaster.BlockBehavior;
 using Vintagestory.API.Common;
 
 namespace MorePlaster;
 
+[SuppressMessage("ReSharper", "UnusedType.Global")]
 public class MorePlasterMod : ModSystem {
     public override void Start(ICoreAPI api) {
         api.RegisterBlockBehaviorClass("MorePlaster.BlockName", typeof(BlockBehaviorName));
     }
-
-    public override void Dispose() { }
 }
